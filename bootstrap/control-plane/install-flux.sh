f315bba38653361679b6fada6aeffd682afedd24
nleahy <56574234+nleahy@users.noreply.github.com>
2025-10-02 10:17:39 -0700

    feat: adding script for flux install

--- bootstrap/control-plane/install-flux.sh
+++ bootstrap/control-plane/install-flux.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 #install flux
+sudo curl -s https://fluxcd.io/install.sh | sudo bash
+
+#create kube config
+sudo cp /etc/rancher/k3s/k3s.yaml .kube/config
+
+#environment variables
+export GITHUB_USER=nleahy
+export REPOSITORY=pi-cluster
+export BRANCH=main
+export PATH=./clusters/staging
+
+#bootstrap flux
+flux bootstrap github --owner=$GITHUB_USER --repository=$REPOSITORY --branch=$BRANCH--path=$PATH --personal
